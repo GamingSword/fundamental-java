184808590a29e345cb60e887a2b420597dc9db12
GamingSword <bdv101@gmail.com>
2016-08-06 18:23:19 -0500

    Update Solution.java

--- Sorting Algorithms/Insertion Sort/Solution.java
+++ Sorting Algorithms/Insertion Sort/Solution.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class Solution {
   public static void main(String[] args) {
-    System.out.print(Arrays.toString(sort(new int[]{3, 4, 1, 5, 11, 17, 7, 12, 2, 9})));
+    System.out.print(Arrays.toString(sort(new int[] {3, 4, 1, 5, 11, 17, 7, 12, 2, 9})));
   }
 
   public static int[] sort(int[] array) {
