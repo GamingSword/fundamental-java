27afb29516de2015f927b8c4ab723e3506e4ddee
GamingSword <bdv101@gmail.com>
2016-08-06 17:54:14 -0500

    Update Solution.java

--- Miscellaneous/FizzBuzz/Solution.java
+++ Miscellaneous/FizzBuzz/Solution.java
@@ -2,12 +2,12 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
-            String text = "";
-            text += i % 3 == 0 ? "Fizz" : "";
-            text += i % 5 == 0 ? "Buzz" : "";
-            System.out.print(text.isEmpty() ? i : text);
-        }
+  public static void main(String[] args) {
+    for (int i = 1; i <= 100; i++) {
+      String text = "";
+      text += i % 3 == 0 ? "Fizz" : "";
+      text += i % 5 == 0 ? "Buzz" : "";
+      System.out.print(text.isEmpty() ? i : text);
     }
+  }
 }
