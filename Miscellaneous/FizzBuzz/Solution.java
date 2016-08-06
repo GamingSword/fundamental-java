03939128bb77d0664fc1cb7bda72e284d1ada723
GamingSword <bdv101@gmail.com>
2016-08-06 17:50:26 -0500

    Create Solution.java

---
+++ Miscellaneous/FizzBuzz/Solution.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            String text = "";
+            text += i % 3 == 0 ? "Fizz" : "";
+            text += i % 5 == 0 ? "Buzz" : "";
+            System.out.print(text.isEmpty() ? i : text);
+        }
+    }
+}
